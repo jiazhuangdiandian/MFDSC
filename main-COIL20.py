ffb953a0f5f9dcfcf253403cc7b7dc7350068f9f
假装点点 <48617720+jiazhuangdiandian@users.noreply.github.com>
2026-05-06 20:21:25 +0800

    Update main-COIL20.py

--- main-COIL20.py
+++ main-COIL20.py
@@ -274,8 +274,7 @@ if __name__ == "__main__":
         dscnet = DSCNet(num_sample=num_sample, channels=channels, kernels=kernels, T=T)
         dscnet.to(device)
 
-        # ae_state_dict = torch.load('D:/5\DSC\pretrained_weights_original/%s-151.pkl' % db)
-        ae_state_dict = torch.load('D:/5\DSC\pretrained_weights_original/%s-147.pkl' % db)
+        ae_state_dict = torch.load('D:/5\DSC\pretrained_weights_original/%s.pkl' % db)
         dscnet.load_state_dict(ae_state_dict)
         print("Pretrained ae weights are loaded successfully.")
         train(dscnet, x, y, epochs, weight_coef=weight_coef, weight_selfExp=weight_selfExp, alpha=alpha, dim_subspace=dim_subspace, ro=ro, show=args.show_freq, device=device)
